a28c56c5a8f6582170df95c7b8407bbf11b104d7
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2025-06-28 01:35:30 +0800

    Commit files for backend
    - created dropwizard boilerplate code
    - added an api to retrieve denomination amounts depending on query string

---
+++ src/main/java/com/github/melvinmelongit/cointargetappbackend/CoinApplication.java
@@ -0,0 +1,15 @@
+package com.github.melvinmelongit.cointargetappbackend;
+
+import io.dropwizard.core.Application;
+import io.dropwizard.core.setup.Environment;
+
+public class CoinApplication extends Application<CoinConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new CoinApplication().run(args);
+    }
+
+    @Override
+    public void run(CoinConfiguration configuration, Environment environment) {
+        environment.jersey().register(new CoinResource());
+    }
+}
---
+++ src/main/java/com/github/melvinmelongit/cointargetappbackend/CoinConfiguration.java
@@ -0,0 +1,8 @@
+package com.github.melvinmelongit.cointargetappbackend;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.core.Configuration;
+import jakarta.validation.constraints.NotEmpty;
+
+public class CoinConfiguration extends Configuration {
+}
